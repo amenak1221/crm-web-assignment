1202e785605b600f919cc9b358f27c33030e5d52
Amena <ame1.khatun@gmail.com>
2017-01-24 19:17:03 -0500

    final commit

--- contact.rb
+++ contact.rb
@@ -1,3 +1,33 @@
+require 'sinatra'
+
+class Contact
+
+
+  attr_accessor :first_name, :last_name, :email, :note
+  attr_reader :id
+  @@contacts = []
+  @@next_id = 1000
+
+
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@next_id
+  end
+
+  #
+  def self.create(first_name, last_name, email, note)
+    #1.initialize a new contact with a unique ID
+    new_contact = self.new(first_name, last_name, email, note)
+    #2. add the new contact to the list of all contacts
+    @@contacts << new_contact
+    #3. Increment the next unique id
+    @@next_id += 1
+    new_contact
+  end
 # Copy your entire Contact class from the old crm assignment here.
 # Then, run the following at the command line:
 #
--- crm.rb
+++ crm.rb
@@ -1,3 +1,35 @@
+require 'sinatra'
+
+require_relative 'contact'
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+end
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
+  @crm_app_name = "Bitmaker's CRM"
+get '/' do
+  erb :index
+end
+
+get '/' do
+  erb :new_contact
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  puts params
+end
+
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
