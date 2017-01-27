d33a9aa4d6de7bd73d4e1b69911618c8118661a6
amenakhatun <ame1.khatun@gmail.com>
2017-01-26 19:29:22 -0500

    Add files via upload
    thursdays assignment completed minus the CSS portion.

--- crm-web-assignment/crm.rb
+++ crm-web-assignment/crm.rb
@@ -1,14 +1,22 @@
 require_relative 'contact'
 require 'sinatra'
 
+# Contact.create('Marty', 'McFly', 'marty@mcfly.com', 'CEO')
+# Contact.create('George', 'McFly', 'george@mcfly.com', 'Co-Founder')
+# Contact.create('Lorraine', 'McFly', 'lorraine@mcfly.com', 'Co-Founder')
+# Contact.create('Biff', 'Tannen', 'biff@tannen.com', 'Co-Founder')
+# Contact.create('Doc', 'Brown', 'doc@brown.com', 'Co-Founder')
+#
+
+
 get '/' do
-  @crm_app_name = "CRM"
+  @crm_app_name = "Amena's CRM"
   erb :index
 end
 
 get '/contacts' do
   @contact_name = 'Contacts'
-  erb :contacts
+  erb :contact
 end
 
 get '/contacts/new' do
@@ -16,6 +24,56 @@ get '/contacts/new' do
 end
 
 post '/contacts' do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  redirect to('/')
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra: :NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    #if the contact exists update the following properties from the params hash (data received from the browser)
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    # action to save a contact in the database
+    @contact.save
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+get '/contacts/1' do
+  @contact = Contact.find(1)
+  erb :show_contact
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
